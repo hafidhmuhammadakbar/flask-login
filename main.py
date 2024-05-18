e244a56e103c20ce0dd7ccfa52d1b9bf636f07fd
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2024-05-18 18:44:25 +0700

    Add more secure hashing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import MySQLdb.cursors, re, hashlib
+import re
+import bcrypt
 import pyotp
 import qrcode
 from io import BytesIO
@@ -18,10 +19,10 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pythonlogin'
 
-# Intialize MySQL
+# Initialize MySQL
 mysql = MySQL(app)
 
-# redirect / to /flasklogin
+# Redirect / to /flasklogin
 @app.route('/')
 def index():
     return redirect(url_for('login'))
@@ -37,14 +38,25 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        # Hash the password
-        hash = password + app.secret_key
-        hash = hashlib.sha1(hash.encode())
-        password = hash.hexdigest()
-
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         account = cursor.fetchone()
+
+        if account and bcrypt.checkpw(password.encode(), account['password'].encode()):
+            session['login_attempts'] = 0  # Reset login attempts on successful login
+            if account['2fa_enabled']:
+                session['2fa_pending'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                return redirect(url_for('two_factor_auth'))
+            else:
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                return redirect(url_for('home'))
+        else:
+            session['login_attempts'] += 1
+            msg = 'Incorrect username/password!'
 
         # Verify reCAPTCHA if the user has failed more than 3 times
         if session['login_attempts'] >= 3:
@@ -66,128 +78,83 @@ def login():
                 msg = 'Invalid reCAPTCHA. Please try again.'
                 return render_template('index.html', error=msg)
 
-        if account:
-            session['login_attempts'] = 0  # Reset login attempts on successful login
-            if account['2fa_enabled']:
-                session['2fa_pending'] = True
-                session['id'] = account['id']
-                session['username'] = account['username']
-                return redirect(url_for('two_factor_auth'))
-            else:
-                session['loggedin'] = True
-                session['id'] = account['id']
-                session['username'] = account['username']
-                return redirect(url_for('home'))
-        else:
-            session['login_attempts'] += 1
-            msg = 'Incorrect username/password!'
-
     return render_template('index.html', error=msg)
 
-# http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
+# Register
 @app.route('/flasklogin/register', methods=['GET', 'POST'])
 def register():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
         name = request.form['name']
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
 
-        # Check if username already exists
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         account = cursor.fetchone()
 
-        # If account exists show error and validation checks
         if account:
             msg = 'Username already exists!'
-            return render_template('register.html', error=msg)
-        
-        # Check if username and email already exists
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
-        account = cursor.fetchone()
-
-        if account:
-            msg = 'Email already exists!'
-            return render_template('register.html', error=msg)
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-            return render_template('register.html', error=msg)
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-            return render_template('register.html', error=msg)
-        elif len(username) < 4:
-            msg = 'Username must be at least 4 characters long!'
-            return render_template('register.html', error=msg)
-        elif len(password) < 8:
-            msg = 'Password must be at least 8 characters long!'
-            return render_template('register.html', error=msg)
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-            return render_template('register.html', error=msg)
         else:
-            # Hash the password
-            hash = password + app.secret_key
-            hash = hashlib.sha1(hash.encode())
-            password = hash.hexdigest()
-            # Account doesn't exist, and the form data is valid, so insert the new account into the users table
-            cursor.execute('INSERT INTO users (name, username, password, email) VALUES (%s, %s, %s, %s)', (name, username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
-            return render_template('index.html', success=msg)
+            cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
+            account = cursor.fetchone()
+
+            if account:
+                msg = 'Email already exists!'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address!'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                msg = 'Username must contain only characters and numbers!'
+            elif len(username) < 4:
+                msg = 'Username must be at least 4 characters long!'
+            elif len(password) < 8:
+                msg = 'Password must be at least 8 characters long!'
+            elif not username or not password or not email:
+                msg = 'Please fill out the form!'
+            else:
+                hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+                cursor.execute('INSERT INTO users (name, username, password, email) VALUES (%s, %s, %s, %s)', (name, username, hashed_password, email,))
+                mysql.connection.commit()
+                msg = 'You have successfully registered!'
+                return render_template('index.html', success=msg)
 
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
+
     return render_template('register.html', error=msg)
 
-# http://localhost:5000/logout - this will be the logout page
+# Logout
 @app.route('/flasklogin/logout')
 def logout():
-    # Remove session data, this will log the user out
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
-    # Redirect to login page
     return redirect(url_for('login'))
 
-# http://localhost:5000/flasklogin/home - this will be the home page, only accessible for logged in users
+# Home
 @app.route('/flasklogin/home')
 def home():
-    # Check if the user is logged in
     if 'loggedin' in session:
         user_id = session['id']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT 2fa_enabled FROM users WHERE id = %s', (user_id,))
         account = cursor.fetchone()
         
-        # Determine if 2FA is enabled
         is_2fa_enabled = account['2fa_enabled'] if account else False
         
-        # Render home template with the 2FA status
         return render_template('home.html', username=session['username'], is_2fa_enabled=is_2fa_enabled)
     
-    # User is not logged in redirect to login page
     return redirect(url_for('login'))
 
-# http://localhost:5000/flasklogin/profile - this will be the profile page, only accessible for logged in users
+# Profile
 @app.route('/flasklogin/profile')
 def profile():
-    # Check if the user is logged in
     if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM users WHERE id = %s', (session['id'],))
         account = cursor.fetchone()
-        # Show the profile page with account info
         return render_template('profile.html', account=account)
-    # User is not logged in redirect to login page
     return redirect(url_for('login'))
 
 @app.route('/flasklogin/2fa')
@@ -225,12 +192,10 @@ def enable_2fa():
             user_id = session['id']
             secret = pyotp.random_base32()
             
-            # Save the 2FA secret to the user's record
             cursor = mysql.connection.cursor()
             cursor.execute('UPDATE users SET 2fa_secret = %s WHERE id = %s', (secret, user_id))
             mysql.connection.commit()
 
-            # Generate QR code
             totp = pyotp.TOTP(secret)
             uri = totp.provisioning_uri(session['username'], issuer_name="Login-Auth-JMPL")
             img = qrcode.make(uri)
@@ -259,7 +224,6 @@ def verify_2fa_enable():
                 mysql.connection.commit()
                 return redirect(url_for('home'))
             else:
-                # Generate QR code and secret again
                 secret = pyotp.random_base32()
                 uri = totp.provisioning_uri(session['username'], issuer_name="YourApp")
                 img = qrcode.make(uri)
@@ -280,4 +244,4 @@ def disable_2fa():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
